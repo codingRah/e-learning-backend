0dcf70db52a4b5fe03104caae47bf0382c84d51c
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-01-24 10:44:17 -0800

    [IMP] method base view for student profile

--- user_profile/api/views/student_profile_view.py
+++ user_profile/api/views/student_profile_view.py
@@ -1,37 +1,47 @@
 from rest_framework.response import Response
-from rest_framework import viewsets
-from user_profile.api.serializers.student_profile_serializer import StudentProfileSerializer
+from rest_framework.decorators import api_view
 from rest_framework import status
+from user_profile.api.serializers.student_profile_serializer import StudentProfileSerializer
 from user_profile.api.models.student_profile_model import StudentProfile
 
 
-class StudentProfileViewSet(viewsets.ViewSet):
-    """view for student profile"""
+@api_view(['POST', ])
+def create_student_profile(request):
+    """Create new student profile"""
+    serializer = StudentProfileSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def list(self, request):
-        pass
+@api_view(['GET', ])
+def student_profile_details(request, pk):
+    """Get single student profile information"""
+    try:
+        student = StudentProfile.objects.get(pk=pk)
+        serializer = StudentProfileSerializer(student)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    except:
+        return Response("The student profile Not Found!", status=status.HTTP_400_BAD_REQUEST)
 
-    def create(self, request):
-        serializer = StudentProfileSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(
-                serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-            )
 
-    def retrieve(self, request, pk=None):
-        queryset = StudentProfile.objects.get(pk=pk)
-        serializer = StudentProfileSerializer(queryset, many=True)
-        return Response(serializer.data)
+@api_view(['PUT', ])
+def update_student_profile(request, pk):
+    """Update a student profile information"""
+    student = StudentProfile.objects.get(pk=pk)
+    serializer = StudentProfileSerializer(StudentProfile, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)    
 
-    def update(self, request, pk=None):
-        pass
-
-    def partial_update(self, request, pk=None):
-        pass
-
-    def destroy(self, request, pk=None):
-        pass
+@api_view(['DELETE', ])
+def delete_student_profile(request, pk):
+    try:
+        student = StudentProfile.objects.get(pk=pk)
+        student.delete()
+        return Response({"message": "Profile info deleted successfully!"})
+    except:
+        return Response("The student profile not found!", status=status.HTTP_400_BAD_REQUEST)
+    
