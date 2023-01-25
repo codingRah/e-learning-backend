0d0bcb61c1c26ef529e3b1b816d07592cd8424e9
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-01-25 16:19:34 +0430

    [IMP] profile image view

--- user_profile/api/urls.py
+++ user_profile/api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
-from .views.user_profile_pic_view import UploadProfileImageViewSet
+from .views.user_profile_pic_view import UserProfileImageView
+from .views import student_profile_view
 
 
 urlpatterns = [
-    path('profile-pic/', UploadProfileImageViewSet, name="profile-pic"),
+    path('std/create/', student_profile_view.create_student_profile, name="create-student-profile"),
+    path('std/<slug:slug>/', student_profile_view.student_profile_details, name="student-profile"),
+    path('std/update/<slug:slug>/', student_profile_view.update_student_profile, name="update-student-profile"),
+    path('std/delete/<slug:slug>/', student_profile_view.delete_student_profile, name="delete-student-profile"),
+    path('profile-pic/', UserProfileImageView.as_view(), name="profile-pic"),
 ]
 
--- user_profile/api/views/user_profile_pic_view.py
+++ user_profile/api/views/user_profile_pic_view.py
@@ -1,16 +1,43 @@
 from rest_framework.response import Response
-from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from rest_framework.views import APIView
+from user_profile.api.models.user_profile_pic_model import UserProfileImage
 from user_profile.api.serializers.user_profile_pic_serializer import UserProfileImageSerializer
 
 
-class UploadProfileImageViewSet(mixins.CreateModelMixin, 
-                                mixins.RetrieveModelMixin, 
-                                mixins.UpdateModelMixin, 
-                                mixins.DestroyModelMixin):
-    serializer_class = UserProfileImageSerializer
-    # permission_classes = [IsAuthenticated]
+class UserProfileImageView(APIView):
     
-    def get_queryset(self):
-        return super().get_queryset()
+    def get(self, request):
+        profile_pic = UserProfileImage.objects.all()
+        serializer = UserProfileImageSerializer(profile_pic)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = UserProfileImageSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    def put(self, request, pk):
+        profile_pic = self.get_object(pk=pk)
+        serializer = UserProfileImageSerializer(profile_pic, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        profile_pic = self.get_object(pk=pk)
+        profile_pic.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def get_object(self, pk):
+        try: 
+            return UserProfileImage.objects.get(pk=pk)
+        except UserProfileImage.DoesNotExist:
+            return Response('The Profile image Not found!', status=status.HTTP_404_NOT_FOUND) 
+
