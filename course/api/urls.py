2ec28286ce33e380de49ca16015e8fcedd5a1302
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-02-03 10:01:13 -0800

    added rul for course enrollment

--- course/api/urls.py
+++ course/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.course_category_view import CourseCategoryView, LanguageView, CourseTypeView
 from .views.course_view import CourseView
+from .views.enrollment_view import EnrollmentView
 
 
 router = DefaultRouter()
@@ -9,6 +10,7 @@ router.register('course-category', CourseCategoryView, basename="course-category
 router.register('course-language', LanguageView, basename="course-language")
 router.register('course-type', CourseTypeView, basename="course-type")
 router.register('course', CourseView, basename="course")
+router.register('enrollment', EnrollmentView, basename="course-enrollment")
 
 
 urlpatterns = [
