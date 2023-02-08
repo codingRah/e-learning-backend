b2a49f722146f9bd126e7a7e2de3f3e99859375c
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-02-08 09:26:18 -0800

    [IMP] course part view and serializer

--- course/api/urls.py
+++ course/api/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views.course_category_view import CourseCategoryView, LanguageView, CourseTypeView
 from .views.course_view import CourseView
 from .views.enrollment_view import EnrollmentView
+from .views.course_part_view import CoursePartView
 
 
 router = DefaultRouter()
@@ -11,6 +12,7 @@ router.register('course-language', LanguageView, basename="course-language")
 router.register('course-type', CourseTypeView, basename="course-type")
 router.register('course', CourseView, basename="course")
 router.register('enrollment', EnrollmentView, basename="course-enrollment")
+router.register('course-part', CoursePartView, basename="course-part")
 
 
 urlpatterns = [
---
+++ course/api/views/course_part_view.py
@@ -0,0 +1,53 @@
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from course.api.models.course_part_model import CoursePart
+from course.api.serializers.course_part_serializer import CoursePartSerializer
+
+
+class CoursePartView(viewsets.ModelViewSet):
+    """view for course parts"""
+    serializer_class = CoursePartSerializer
+    queryset = CoursePart.objects.all()
+    permission_classes = (IsAuthenticated, )
+
+    def list(self, request):
+        serializer = CoursePartSerializer(self.queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def retrieve(self, request, pk=None):
+        course_part = get_object_or_404(self.queryset, pk=pk)
+        serializer = CoursePartSerializer(course_part)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def create(self, request):
+        serializer = CoursePartSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        course_part = get_object_or_404(self.queryset, pk=pk)
+        serializer = CoursePartSerializer(course_part, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        course_part = get_object_or_404(self.queryset, pk=pk)
+        serializer = CoursePartSerializer(course_part, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        course_part = get_object_or_404(self.queryset, pk=pk)
+        course_part.delete()
+        return Response({"message": "Course part deleted successfully!"}, status=status.HTTP_204_NO_CONTENT)
