8df6caceb0498427dd087ac845e9c28527321f17
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-02-10 09:40:19 +0430

    models added in admin panel

--- course/admin.py
+++ course/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .api.models.assignment_model import CourseAssignment
+from .api.models import assignment_model, course_attachment_model, comments_model,course_category_model, course_model, course_part_model,course_video_model
 
 
-admin.site.register(CourseAssignment)
+admin.site.register(assignment_model.CourseAssignment)
+admin.site.register(course_attachment_model.CourseAttachment)
+admin.site.register(comments_model.CourseComment)
+admin.site.register(course_category_model.CourseCategory)
+admin.site.register(course_model.Course)
+admin.site.register(course_part_model.CoursePart)
+admin.site.register(course_video_model.CourseVideo)
