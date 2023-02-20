6bf4de59b4e48767172ed17909e34e9b9e3a9432
Ali-hadi1 <hadiadelzada2011@gmail.com>
2023-02-20 16:17:30 +0430

    [ADDED] a field in serizlizer

--- course/api/serializers/course_serializer.py
+++ course/api/serializers/course_serializer.py
@@ -1,13 +1,15 @@
 from rest_framework import serializers
 from course.api.models.course_model import Course
+from course.api.serializers.comment_serializer import CommentSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
     """course serializer"""
 
+    comments = CommentSerializer(many=True, read_only=True)
     class Meta:
         model = Course
-        fields = ['id', 'title', 'description', 'category', 'start_date', 'end_date', 'start_time', 'end_time', 'course_type', 'language', 'created_by']
+        fields = ['id', 'title', 'description', 'category', 'start_date', 'end_date', 'start_time', 'end_time', 'course_type', 'language', 'created_by', 'comments']
 
         extra_kwargs = {
             'created_by': {'write_only': True}
